a0424f1da8bafe54227efe11f978d3183536681a
alexandrejastrow <alexandre.jastrow@gmail.com>
2022-04-09 12:53:08 -0300

    create app pages, folder templates, an user migration allauth

---
+++ pages/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from django.views import View
+# Create your views here.
+from places.models import CategoryPlace, Place
+
+
+class HomePageView(View):
+    template_name = 'home.html'
+
+    def get(self, request, *args, **kwargs):
+        categories = CategoryPlace.objects.all()
+        places = Place.objects.all()
+        return render(request, self.template_name, {'categories': categories, 'places': places})

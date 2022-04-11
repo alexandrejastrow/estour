87db898346cc6704b67cd76a783755f1dc5abc0d
alexandrejastrow <alexandre.jastrow@gmail.com>
2022-04-10 21:22:49 -0300

    monitor

--- estour/settings/heroku.py
+++ estour/settings/heroku.py
@@ -1,3 +1,5 @@
+from sentry_sdk.integrations.django import DjangoIntegration
+import sentry_sdk
 import environ
 from estour.settings.base import *
 
@@ -19,3 +21,18 @@ DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 DATABASES = {
     'default': env.db(),
 }
+
+
+sentry_sdk.init(
+    dsn="https://9d132ac8a1834e2d804e06e4409314e2@o1198631.ingest.sentry.io/6321155",
+    integrations=[DjangoIntegration()],
+
+    # Set traces_sample_rate to 1.0 to capture 100%
+    # of transactions for performance monitoring.
+    # We recommend adjusting this value in production.
+    traces_sample_rate=1.0,
+
+    # If you wish to associate users to errors (assuming you are using
+    # django.contrib.auth) you may enable sending PII data.
+    send_default_pii=True
+)
--- estour/urls.py
+++ estour/urls.py
@@ -18,7 +18,13 @@ from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
 
+
+def trigger_error(request):
+    division_by_zero = 1 / 0
+
+
 urlpatterns = [
+    path('sentry-debug/', trigger_error),
     path('accounts/', include('allauth.urls')),
     path('admin/', admin.site.urls),
     path('', include('pages.urls')),

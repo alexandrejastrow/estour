49d80e88e6bd1783d1fce245332d8328a60db8b9
alexandrejastrow <alexandre.jastrow@gmail.com>
2022-04-15 19:33:32 -0300

    refatore project, create models: places, gallery, image, category, rating, favoritelist

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,9 +1,14 @@
 from django.contrib import admin
 from django.contrib.auth import admin as auth_admin
 
-from .models import User
+from .models import User, FavoriteList
 
 
 @admin.register(User)
 class UserAdmin(auth_admin.UserAdmin):
+    pass
+
+
+@admin.register(FavoriteList)
+class FavoriteAdmin(admin.ModelAdmin):
     pass
---
+++ places/admin.py
@@ -0,0 +1,28 @@
+from django.contrib import admin
+
+from .models import Place, CategoryPlace, Rating, Gallery, Photo
+
+
+@admin.register(Place)
+class PlaceAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(CategoryPlace)
+class CategoryPlaceAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Rating)
+class RatingAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Gallery)
+class GalleryAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Photo)
+class PhotoAdmin(admin.ModelAdmin):
+    pass

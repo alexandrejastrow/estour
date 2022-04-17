3155855c8be30c8925514b3b2d1df8d7231fbe06
alexandrejastrow <alexandre.jastrow@gmail.com>
2022-04-17 00:14:46 -0300

    refatore project, add section categories, destaques and footer on home page

--- static/js/header.js
+++ static/js/header.js
@@ -1,9 +1,30 @@
 const hamburger = document.querySelector('.hamburger');
 const navMenu = document.querySelector('.nav-menu');
+const main = document.querySelector('.main-content');
 
+const sizes = {
+    "482": '282px',
+    "1197": '282px',
+};
+
+function changeMarginTop(value) {
+
+    if (main.style.marginTop === '') {
+        main.style.marginTop = value
+    } else {
+        main.style.marginTop = ""
+    }
+}
 hamburger.addEventListener('click', () => {
+
+    let windowWidth = window.innerWidth;
+
     hamburger.classList.toggle('active');
     navMenu.classList.toggle('active');
+
+    if (windowWidth < 1197) {
+        changeMarginTop("282px")
+    }
 });
 
 function search_function() {

27cdaed5cb41e3c7f1c880e90c34f2cb0cbb762b
alexandrejastrow <alexandre.jastrow@gmail.com>
2022-04-19 20:19:36 -0300

    remove centry

--- core/settings/heroku.py
+++ core/settings/heroku.py
@@ -1,5 +1,4 @@
 from sentry_sdk.integrations.django import DjangoIntegration
-import sentry_sdk
 import environ
 from core.settings.base import *
 
@@ -24,20 +23,6 @@ DATABASES = {
 }
 
 
-sentry_sdk.init(
-    dsn="https://9d132ac8a1834e2d804e06e4409314e2@o1198631.ingest.sentry.io/6321155",
-    integrations=[DjangoIntegration()],
-
-    # Set traces_sample_rate to 1.0 to capture 100%
-    # of transactions for performance monitoring.
-    # We recommend adjusting this value in production.
-    traces_sample_rate=1.0,
-
-    # If you wish to associate users to errors (assuming you are using
-    # django.contrib.auth) you may enable sending PII data.
-    send_default_pii=True
-)
-ROOT_URLCONF = 'core.urls'
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
 

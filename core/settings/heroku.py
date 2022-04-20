3febdd571938f84e6095bc1d232f24e69bc654c0
alexandrejastrow <alexandre.jastrow@gmail.com>
2022-04-19 21:18:00 -0300

    remove email smtp

--- core/settings/heroku.py
+++ core/settings/heroku.py
@@ -24,13 +24,4 @@ DATABASES = {
 }
 
 
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-
-
-# email
-DEFAULT_FROM_EMAIL = env('DEFAULT_FROM_EMAIL')
-EMAIL_HOST_USER = env('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = env('EMAIL_HOST_PASSWORD')
-EMAIL_USE_TLS = True
-EMAIL_PORT = 587
-EMAIL_HOST = env('EMAIL_HOST')
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
